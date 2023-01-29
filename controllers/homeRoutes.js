74204e46e5006602a49119d990af5574fb0bd460
Joshua <janorris65@gmail.com>
2023-01-29 17:57:24 -0600

    create functioning logout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,28 +5,38 @@ const withAuth = require("../utils/auth");
 router.get("/", async (req,res) => {
     const blogData = await Blog.findAll();
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
-    console.log(blogs, "hey11111");
-    res.render('homepage',{ blogs })
+    console.log(blogs, "hey!!!!");
+    res.render('homepage',{ blogs,
+        loggedIn: req.session.logged_in
+      });
     // contains all blog posts, needs creator names, and click access to update
 })
 
-router.get("/blogpost/:id", async (req,res)=> {
-    res.render("blogpost")
+router.get("/blogpost/", async (req,res)=> {
+    // res.render("blogpost")
+    // const userblogData = await Blog.findAll({
+    //     where: {
+    //         Userid: 
+    //     }
+    // })
     // render clicked blogpost, option to leave comment
 })
 
 router.get("/login", async (req,res)=> {
-    res.render("login")
-    // sign in, still need sign up (leads to /signup)
+    res.render("login") 
+    // sign in, still need sign up (leads to /signup) done 
 })
 
 router.get("/signup", async (req,res)=> {
     res.render("signup")
-    // sign up (from login)
+    // sign up (from login), done 
 })
 
 router.get("/dashboard", withAuth, async (req,res)=> {
-    res.render("dashboard")
+    res.render("dashboard",{
+        loggedIn: req.session.logged_in
+      });
+    console.log("yo!!!!");
     //contains my blog posts and options to add new (title and content),return to dashboard
 })
 
