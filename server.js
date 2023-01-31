ed2c3086fa803a1c516a6ec0a500e29d38a78b9b
Joshua <janorris65@gmail.com>
2023-01-31 00:23:29 -0600

    heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import { join } from 'path';
 import express, { json, urlencoded } from 'express';
 import session, { Store } from 'express-session';
 import { create } from 'express-handlebars';
-import routes from './controllers';
+import routes from './controllers/index';
 import helpers from './utils/helpers';
 
 import sequelize, { sync } from './config/connection';
