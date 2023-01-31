1108d7e253778c26c43ea39cfc4f08f6cdc9258d
Joshua <janorris65@gmail.com>
2023-01-31 00:12:20 -0600

    es module

---
+++ server.js
@@ -0,0 +1,39 @@
+import { join } from 'path';
+import express, { json, urlencoded } from 'express';
+import session, { Store } from 'express-session';
+import { create } from 'express-handlebars';
+import routes from './controllers';
+import helpers from './utils/helpers';
+
+import sequelize, { sync } from './config/connection';
+const SequelizeStore = require('connect-session-sequelize')(Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: { maxAge: 24 * 60 * 60 * 1000,},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use((join(__dirname, 'public')));
+
+app.use(routes);
+
+sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on PORT 3001'));
+});
